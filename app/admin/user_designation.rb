ac1d1b6e4ced1f0bdc8f8a0797956d2c360e9ce6
Jason Mabry <jmabry@mymabry.com>
2016-01-27 12:33:43 -0500

    attempt user designation admin form

--- app/admin/user_designation.rb
+++ app/admin/user_designation.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register UserDesignation do
-  
+  index do
+    column :school
+    column :user
+  end
+  form do |f|
+    f.inputs "User Designation Details" do
+      f.input :school
+      f.input :user
+    end
+    f.actions
+  end
 end
